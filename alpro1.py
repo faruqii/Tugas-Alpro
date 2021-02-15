232b247c25cf0fff3af4b12d08ba274546d26588
Faruqi Hafiz <68532485+faruqii@users.noreply.github.com>
2021-02-15 17:40:31 +0700

    Create alpro1.py
    tugas membuat dara diri mahasiswa

---
+++ alpro1.py
@@ -0,0 +1,9 @@
+nama = input("Nama : ")
+asal = input("Asal : ")
+jurusan = input("Jurusan : ")
+kelas = input("kelas : ")
+hobi = input("hobi : ")
+suka = input("suka : ")
+
+print("Halo! Perkenalkan saya",nama "Saya berasal dari",asal 
+      "Jurusan saya di Telkom University adalah",jurusan "di kelas",kelas "saya mempunyai hobi",hobi "Saya tertarik kepada",suka "Salam Kenal teman-teman semua")
