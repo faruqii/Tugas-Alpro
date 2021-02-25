5bd5c88b552bdead01e08657b0e995f1045991f3
Faruqi Hafiz <68532485+faruqii@users.noreply.github.com>
2021-02-25 16:58:30 +0700

    Create statistic2.py

---
+++ statistic2.py
@@ -0,0 +1,24 @@
+# list = [100,80,55,70,25,40,100,20,90,50]
+import statistics
+import numpy as np
+
+list = []
+n = int(input("masukan jumlah data :"))
+
+for i in range(0, n):
+    element = int(input())
+
+    list.append(element)
+    
+list.sort()
+average = statistics.mean(list)
+median = statistics.median(list)
+modus = statistics.mode(list)
+
+print("Nilai Rata-Rata :",average)
+print("Median :",median)
+print("Modus :",modus)
+
+print("Q1 :", np.quantile(list, .25 ))
+print("Q2 :", np.quantile(list, .50))
+print("Q3 :", np.quantile(list, .75)) 
